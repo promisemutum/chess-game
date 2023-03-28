a4010bfc1d31db8ce6661fb79c191db15775f1c3
Promise Mutum <problaze28@gmail.com>
2023-03-28 16:37:50 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Chessboard from './components/chessboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Chessboard/>
+    </div>
+  );
+}
+
+export default App;
